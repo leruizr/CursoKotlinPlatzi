6a3d96632a60c6aa3e5a397803fb7517112aedad
leruizr <leruizres1986@gmail.com>
2024-04-10 14:13:17 -0500

    ejercicios bucles y capturando errores

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -25,4 +25,64 @@ fun main(args: Array<String>) {
 
     // Llamar al método analizarCodigo de EstructurasControl
     estructurasControl.analizarCodigo(code)
+
+    //Uso del while
+
+    var contador = 10
+    while (contador > 0){
+        println("el valor del contador es $contador")
+        contador--
+    }
+
+    //Uso del do while
+
+    do {
+        println("Generando numero aleatorio...")
+        val numeroAleatorio = (0..100).random()
+        println("El numero generado es $numeroAleatorio")
+    } while(numeroAleatorio > 50)
+
+    //Uso del for
+
+    val listaDeFrutas = listOf("Manzana", "Pera", "Frambuesa", "Durazno")
+    for (fruta in listaDeFrutas) println("Hoy voy a comer una fruta llamada $fruta")
+
+    //Uso del for each
+
+    listaDeFrutas.forEach { fruta -> println("Mañana voy a comer una fruta nueva llamada $fruta") }
+
+    //Uso del try catch
+
+    var alias : String? = null
+    try {
+        alias!!.length
+
+    } catch (excepcion : NullPointerException){
+        println("Ha ocurrido un error")
+    } finally {
+        println("Finalmente ha ocurrido un error.. Cerrando aplicación")
+    }
+
+    val primerValor = 10
+    val segundoValor = 0
+
+    val resultado : Int = try { primerValor / segundoValor } catch  (exception: Exception) { 0 }
+    print(resultado)
+
+    println(" ")
+
+    //Usando Elvis operador
+
+    println("Practicando operador Elvis")
+    val m: String? = null
+    val l: Int = if (m != null) m.length else -1
+    println(l)
+
+    //Con el operador Elvis expresamos
+    // lo mismo que en la línea anterior
+    val c = m?.length ?: -1
+    println(c)
+
+
+
 }
